ddebf9acc18f244e33cf3308739f0f017e4c6cdf
desoga <done2stay@gmail.com>
2018-09-30 19:20:23 +0100

    Completed design of website

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-alert('we are not ugly');
+/* Function for changing colors of technology bars */
+
+
+function htmlFunction() {
+    var x = document.getElementById('htmls');
+    if (x.style.backgroundColor === 'red') {
+        x.style.backgroundColor = 'blue';
+    } else {
+        x.style.backgroundColor = 'red';
+    }
+}
+
+
+function cssFunction() {
+    var x = document.getElementById('css2');
+    if (x.style.backgroundColor === '#9900ff') {
+        x.style.backgroundColor = '#b84dff';
+    } else {
+        x.style.backgroundColor = '#1f0033';
+    }
+}
+
+
+function javascriptFunction() {
+    var x = document.getElementById('javascript');
+    if (x.style.backgroundColor === '#00ffff') {
+        x.style.backgroundColor = '#b84dff';
+    } else {
+        x.style.backgroundColor = '#00ffff';
+    }
+}
+
+function wordpressFunction() {
+    var x = document.getElementById('wordpress');
+    if (x.style.backgroundColor === '#ff66ff') {
+        x.style.backgroundColor = '#ff00ff';
+    } else {
+        x.style.backgroundColor = '#ff66ff';
+    }
+}
